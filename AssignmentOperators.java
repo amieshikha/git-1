9365520957e229acf4f204a7aa2bc5fad75f00a9
Shikha <101756435+amieshikha@users.noreply.github.com>
2024-02-28 20:01:14 -0500

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,9 @@
+class AssignmentOperators
+{
+    public static void main(String args[])
+     {
+        int num1 = 6;
+        int result= ++num1; //pre increment
+        System.out.println(result);
+    }
+}
